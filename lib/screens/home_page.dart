6e408de5deea62b16473bb6a15c8ec94e5507f32
Senghuy <senghuymit007@gmail.com>
2020-12-12 16:28:56 +0700

    complete

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,50 @@
+import '../widgets/home_page_widgets/home_categories.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      resizeToAvoidBottomPadding: false,
+      appBar: AppBar(
+        //TODO: Add a drawer window
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.menu,
+            color: Colors.black,
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.info_outline,
+              color: Colors.black,
+            ),
+          )
+        ],
+        centerTitle: true,
+        title: Text(
+          "Covid-19 Tracker",
+          style: TextStyle(
+            fontSize: 20,
+            fontFamily: "Montserrat",
+            color: Colors.black,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+      body: HomeCategories(),
+    );
+  }
+}
